6af54e117bfd942f09aa37feb70dc2c0496245aa
j-rewerts <rewerts@ualberta.ca>
2015-11-22 13:09:25 -0700

    Using the matlab packaged logistic regression. It doesn't work well yet.

--- classification/learnLogisticReg.m
+++ classification/learnLogisticReg.m
@@ -1,38 +1,10 @@
 function [ model ] = learnLogisticReg( X, Y )
 
-% In the assignment, we did  cross-val to find the best lambda. I'll leave
-% that out for now.
-lambda = 0.3;
+%catArray = categorical(Y + 1);
 
-% I'm guessing there is a better way to find the best value of this:
+% This can spit out a warning about the matrix being singular. I think this
+% is to do with our class sizes. We don't have very many positives.
+% http://www.mathworks.com/matlabcentral/newsreader/view_thread/171400
+model = mnrfit(X, Y + 1);
 
-% Initialize fitting parameters
-initial_theta = zeros(size(X, 2), 1);
-% Set Options
-options = optimset('GradObj', 'on', 'MaxIter', 400);
-
-% Optimize
-[theta, ~, ~] = ...
-fminunc(@(t)(costFunction(t, X, Y, lambda)), initial_theta, options);
-
-model = theta;
 end
-
-function [J, grad] = costFunction(theta, X, y, lambda)
-
-m = length(y); % number of training examples
-
-% Return these
-J = 0;
-grad = zeros(size(theta));
-
-h = sigmoid(X * theta);
-newTheta = theta;
-newTheta(1) = 0;
-
-J = (1/m) * sum(-y .* log(h) - (1 - y) .* log(1 - h)) + ...
-    (lambda / (2*m) * sum(newTheta .^ 2));
-
-grad = (1/m) * ((X' * (h - y)) + lambda * newTheta);
-end
-
