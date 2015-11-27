5666440551f1d48cedcf2f2dae544779af27e0cc
sinaj <sina.jalali.mspc@gmail.com>
2015-11-27 13:36:28 -0700

    Debug 3D Image Edges

--- graph-cut/maxflow-v3.0/edges4connected3Dimage.m
+++ graph-cut/maxflow-v3.0/edges4connected3Dimage.m
@@ -13,25 +13,50 @@ function E = edges4connected3Dimage(height,width,depth)
 %   $Date$
 %
 
-N = height*width*depth;
-I = []; J = [];
-% connect vertically (down, then up)
-is = [1:N]';
-is([height:height:N])=[];
-js = is+1;
-I = [I;is;js];
-J = [J;js;is];
-% connect horizontally (right, then left)
-is = [1:N-height]';
-js = is+height;
-I = [I;is;js];
-J = [J;js;is];
-%connect in z dimention
-is = [1:N-width*height]';
-js = is+width*height;
-I = [I;is;js];
-J = [J;js;is];
+s = [width, height, depth];
+w = width;
+h = height;
+d = depth;
+subs = zeros(3*w*h*d - w*h - h*d - d*w, 6);
+c = 1;
 
-E = [I,J];
+for d = 1:depth
+    for h = 1:height
+        for w = 1:width-1
+            subs(c, :) = [w h d (w+1) h d];
+            c = c+1;
+%             E = cat(1, E, ( [sub2ind(s, w, h, d) sub2ind(s, w+1, h, d)] ));
+        end
+    end
+end
+
+
+    
+for d = 1:depth
+    for h = 1:height-1
+        for w = 1:width
+            subs(c, :) = [w h d w (h+1) d];
+            c = c+1;
+%             E = cat(1, E, ( [sub2ind(s, w, h, d) sub2ind(s, w, h+1, d)] ));
+        end
+    end
+end
+
+
+    
+for d = 1:depth-1
+    for h = 1:height
+        for w = 1:width
+            subs(c, :) =[w h d w h (d+1)];
+            c = c+1;
+%             E = cat(1, E, ( [sub2ind(s, w, h, d) sub2ind(s, w, h, d+1)] ));
+        end
+    end
+end
+
+starts = sub2ind(s, subs(:, 1), subs(:, 2), subs(:, 3));
+ends = sub2ind(s, subs(:, 4), subs(:, 5), subs(:, 6));
+
+E = [[starts, ends]; [ends, starts]];
 
 end
